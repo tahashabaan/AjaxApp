10943f3397fbd0b852305ccb1b95e099b1368443
tahashabaan <tahashabaan48@gmail.com>
2024-03-19 13:18:05 +0200

    update project

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,152 +1,141 @@
-const listOfPostsEle = document.querySelector('.posts');
-const btnFetchPosts =listOfPostsEle.previousElementSibling;
-const tempPost = document.getElementById('single-post');
-const newPostEle = document.getElementById('new-post');
-const form = newPostEle.querySelector('form');
+const listOfPostsEle = document.querySelector(".posts");
+const btnFetchPosts = listOfPostsEle.previousElementSibling;
+const tempPost = document.getElementById("single-post");
+const newPostEle = document.getElementById("new-post");
+const form = newPostEle.querySelector("form");
 
+const userData = localStorage.getItem("userData");
 
-const userData = localStorage.getItem('userData');
+let data = [];
+data = userData ? data.push(JSON.parse(userData)) : [];
+// data = userData? data.push(JSON.parse(userData)) : [];
 
+console.log("data", data);
 
-let data= [];
-data = userData?
-      data.push(JSON.parse(userData)):[];
-
-      console.log(data)
-
-
-const dataCookie = document.cookie.split(';');
+// convert string to arr by ; in cookie document.cookie.split(';');
+const dataCookie = document.cookie.split(";");
 console.log(dataCookie[0].title);
 
-const htp = async(method, url) => {
-    return await method(url);
-}
+const htp = async (method, url) => {
+  return await method(url);
+};
 
-const indexDb = () =>{
-   const dbReg= indexedDB.open('storageDummy', 1);
+// indexDb
+const indexDb = () => {
+  const dbReg = indexedDB.open("storageDummy", 1);
 
+  dbReg.addEventListener("success", (e) => {
+    const db = e.target.result;
+    db.createObjrctStore("products", { keyPath: "id" });
+  });
 
-   dbReg.addEventListener('success', (e)=>{
-    const db = (e.target.result);
-    db.createObjrctStore('products',{keyPath:'id'})
-   })
+  dbReg.addEventListener("error", (e) => {
+    console.log(e.target.result);
+  });
+};
 
-   dbReg.addEventListener('error', (e)=>{
-    console.log(e.target.result)
-   })
+// create post
+const createPost_2 = () => {
+  const response = htp(axios.get, "https://jsonplaceholder.typicode.com/posts");
 
+  response.then((result) => console.log(result));
+};
 
-}
-
-
-const createPost_2 =() => {
-
- const response =  htp(axios.get,'https://jsonplaceholder.typicode.com/posts');
-
- response.then( result => console.log(result));
-
-}
-
-createPost_2()
-
-
-
-
-
-
-const httpRequest = (method, url, data='') => {
-  const promise = new Promise((resolve, reject) =>
-  {
+// createPost_2();
+// open session connect with jsonplaceholder!!! ajax
+const httpRequest = (method, url, data = "") => {
+  const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json'
+    xhr.responseType = "json";
     xhr.open(method, url);
-    
-    xhr.addEventListener('load', () =>{
-       resolve(xhr.response)
-    })
+
+    xhr.addEventListener("load", () => {
+      resolve(xhr.response);
+    });
 
     xhr.send(JSON.stringify(data));
-  })
-  
+  });
+
   return promise;
-}
- const createPost = async (e) => {
-      e.preventDefault();
-      const userId = Math.random();
-      const title = newPostEle.querySelector('#title').value;
-      const body = newPostEle.querySelector('#content').value
+};
 
-      console.log(title, body)
+const createPost = async (e) => {
+  e.preventDefault();
+  const userId = Math.random();
+  let titleElement = newPostEle.querySelector("#title");
+  let bodyElement = newPostEle.querySelector("#content");
 
-      const  store = {
-        title,
-        body,
-        id:Math.random().toString()
-      }
+  let title = titleElement.value;
+  let body = bodyElement.value;
 
-      localStorage.setItem('userData', JSON.stringify(store))
+  console.log(title, body);
 
-      document.cookie = `id=${JSON.stringify(store)}`
+  // console.log(title, body);
 
-    const resDate =  await httpRequest(
-    'POST',
-    'https://jsonplaceholder.typicode.com/posts'  ,
-    {userId, title, body});
+  const store = {
+    title,
+    body,
+    id: Math.random().toString(),
+  };
 
-    console.log(resDate)
- }
+  localStorage.setItem("userData", JSON.stringify(store));
 
- const fetchPosts = async () => {
+  document.cookie = `id=${JSON.stringify(store)}`;
 
- 
-  let listOfPosts= await httpRequest(
-  'GET', 
-  'https://jsonplaceholder.typicode.com/posts');
+  const resDate = await httpRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    store
+  );
+  titleElement.value = "";
+  bodyElement.value = "";
+  // const res = JSON.parse(resDate);
+  console.log(resDate);
+  alert("Post Created");
+};
 
-   const len = listOfPosts.length;
+const fetchPosts = async () => {
+  let listOfPosts = await httpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
 
-    for(post of listOfPosts) { 
+  const len = listOfPosts.length;
 
-      const postEle = document.importNode(tempPost.content, true);
-     
-      postEle.querySelector('li').id=post.id;
+  for (post of listOfPosts) {
+    const postEle = document.importNode(tempPost.content, true);
 
-      // const btnDel = postEle.querySelector('button')
-      // btnDel.addEventListener('click',deletePost.bind(null, post.id));
+    postEle.querySelector("li").id = post.id;
 
-      postEle.querySelector('h2').textContent = post.title.toUpperCase();
-      postEle.querySelector('p').textContent = post.body;
-    
-      listOfPostsEle.append(postEle);
+    // const btnDel = postEle.querySelector('button')
+    // btnDel.addEventListener('click',deletePost.bind(null, post.id));
 
-    }
- 
-}
+    postEle.querySelector("h2").textContent = post.title.toUpperCase();
+    postEle.querySelector("p").textContent = post.body;
 
-listOfPostsEle.addEventListener('click' , async(e) =>{
-  if(e.target.tagName === 'BUTTON'){
-   const postId = e.target.closest('li').id;
-   await httpRequest(
-     'DELETE',
-     `https://jsonplaceholder.typicode.com/posts/${postId}`)
+    listOfPostsEle.append(postEle);
   }
-})
+};
 
-const deletePost = async (id) =>{
+listOfPostsEle.addEventListener("click", async (e) => {
+  if (e.target.tagName === "BUTTON") {
+    const postId = e.target.closest("li").id;
+    await httpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+    alert("Post Deleted");
+    fetchPosts();
+  }
+});
 
-   await httpRequest(
-    'DELETE',
-    `https://jsonplaceholder.typicode.com/posts/${id}`)
+const deletePost = async (id) => {
+  await httpRequest(
+    "DELETE",
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+};
 
-}
-
-btnFetchPosts.addEventListener('click',
- fetchPosts);
-form.addEventListener('submit', createPost)
-
-
-
-
-
-
+btnFetchPosts.addEventListener("click", fetchPosts);
+form.addEventListener("submit", createPost);
